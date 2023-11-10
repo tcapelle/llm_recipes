c5886cf633684ef7f362f7bb5f880109ecfefd7c
Thomas Capelle <thomas.capelle@steady-sun.com>
2023-11-10 18:21:11 +0000

    shards

--- scripts/tiny_train.py
+++ scripts/tiny_train.py
@@ -8,17 +8,46 @@ from transformers import AutoTokenizer, TrainingArguments
 from transformers.models.llama import LlamaConfig, LlamaForCausalLM
 from trl import SFTTrainer
 
-from llm_recipes.utils import get_latest_file, LLMSampleCB, token_accuracy, param_count
+from llm_recipes.utils import get_latest_file, LLMSampleCB, token_accuracy, param_count, parse_args
 
 WANDB_PROJECT = "tinyllama"
 ENTITY = "capecape"
 DATASET_NAME = "togethercomputer/RedPajama-Data-1T-Sample"
+LAST_CHECKPOINT = None
+TOTAL_DATASET_SHARDS = 20
 
-ds = load_dataset(DATASET_NAME)["train"]
+config = SimpleNamespace(
+    resume_from_checkpoint=LAST_CHECKPOINT,  # uses LAST Checkpoint from W&B
+    use_flash_attention_2=True,
+    torch_compile=True,
+    batch_size=48,
+    learning_rate=5e-4,
+    gradient_accumulation_steps=2,
+    eval_steps=1000, # save model here
+    dataloader_num_workers=6,
+    dataset_shard=0,
+    test_size=0.05,
+    seed=42,
+)
 
-splitted_ds = ds.train_test_split(test_size=0.05, seed=42)
+parse_args(config)
 
-# around 0.5B params
+def get_datasets(ds_name, total_shards=1, shard=0, seed=42, test_size=0.05):
+    "Let's shard so resuming is easier"
+    ds = load_dataset(DATASET_NAME)["train"]
+    splitted_ds = ds.shuffle(seed=seed).train_test_split(test_size=test_size, seed=seed)
+    train_ds = splitted_ds["train"].shard(total_shards, shard)
+    eval_ds = splitted_ds["test"].shard(total_shards, shard)
+    return train_ds, eval_ds
+
+train_ds, eval_ds = get_datasets(
+    DATASET_NAME,
+    total_shards=TOTAL_DATASET_SHARDS,
+    shard=config.dataset_shard,
+    seed=config.seed,
+    test_size=config.test_size)
+
+# around 0.3B params
 model_config = SimpleNamespace(
     dim = 1024,
     multiple_of = 256,
@@ -26,6 +55,7 @@ model_config = SimpleNamespace(
     num_hidden_layers = 16,
     max_seq_len=1024,
     torch_dtype=torch.bfloat16,
+    use_flash_attention_2=config.use_flash_attention_2,
 )
 
 def create_tiny_llama(config):
@@ -48,6 +78,18 @@ def create_tiny_llama(config):
 
 model, llama_config = create_tiny_llama(model_config)
 
+
+## MFU computations
+N = sum(p.numel() for p in self.parameters())
+L, H, Q, T = cfg.num_hidden_layers, cfg.num_attention_heads, cfg.dim//cfg.num_attention_heads, cfg.max_seq_len
+flops_per_token = 6*N + 12*L*H*Q*T
+flops_per_fwdbwd = flops_per_token * T
+flops_per_iter = config.batch_size * config.gradient_accumulation_steps * fwdbwd_per_iter
+
+def flops(dt)
+    return flops_per_iter * (1.0/dt)
+#####################
+
 param_count(model)
 
 def load_tokenizer(model_id = 'meta-llama/Llama-2-7b-hf'):
@@ -58,37 +100,39 @@ def load_tokenizer(model_id = 'meta-llama/Llama-2-7b-hf'):
 tokenizer = load_tokenizer()
 
 # estimation
-total_num_steps = int(1.2e12//1024)
-eval_steps = 1000 # same as save steps
-save_steps = eval_steps
+effective_batch_size = 1024*config.batch_size*config.gradient_accumulation_steps
+total_num_steps = int(1.2e12)//effective_batch_size
+print(f"\nTraining for {total_num_steps} with an effective batch_size of: {effective_batch_size}\n")
 
 output_dir = "/tmp/transformers"
 training_args = TrainingArguments(
     output_dir=output_dir,
-    per_device_train_batch_size=40,
-    per_device_eval_batch_size=40,
+    per_device_train_batch_size=config.batch_size,
+    per_device_eval_batch_size=config.batch_size,
     bf16=True,
-    learning_rate=2e-4,
+    dataloader_num_workers=config.dataloader_num_workers,
+    torch_compile=config.torch_compile,
+    learning_rate=config.learning_rate,
     lr_scheduler_type="cosine",
     warmup_steps=total_num_steps // 100,
     max_steps=total_num_steps,
-    gradient_accumulation_steps=4,
+    gradient_accumulation_steps=config.gradient_accumulation_steps,
     gradient_checkpointing=True,
     evaluation_strategy="steps",
-    eval_steps=eval_steps,
+    eval_steps=config.eval_steps,
     # logging strategies
     logging_dir=f"{output_dir}/logs",
     logging_strategy="steps",
     logging_steps=1,
     save_strategy="steps",
-    save_steps=save_steps,
+    save_steps=config.eval_steps,
 )
 
 trainer = SFTTrainer(
     model,
     tokenizer=tokenizer,
-    train_dataset=splitted_ds["train"],
-    eval_dataset=splitted_ds["test"],
+    train_dataset=train_ds,
+    eval_dataset=eval_ds,
     dataset_text_field="text",
     packing=True,
     max_seq_length=1024,
@@ -134,12 +178,17 @@ class LLMSampleCB(WandbCallback):
         super().on_evaluate(args, state, control, **kwargs)
         self.log_generations_table(self.sample_dataset)        
 
-wandb.init(project=WANDB_PROJECT, entity=ENTITY, job_type="train")
-
-# we have to pass the callback to the training func!
-wandb_callback = LLMSampleCB(trainer, splitted_ds["test"], num_samples=10, max_new_tokens=128)
+wandb.init(project=WANDB_PROJECT, entity=ENTITY, job_type="train", tags=[f"shard_{config.dataset_shard}"])
+wandb_callback = LLMSampleCB(trainer, eval_ds, num_samples=20, max_new_tokens=128)
 trainer.add_callback(wandb_callback)
 
-trainer.train()
-
-wandb.finish()
+if isinstance(config.resume_from_checkpoint, str):
+    artifact = wandb.use_artifact(LAST_CHECKPOINT, type='model')
+    checkpoint_dir = artifact.download()
+    trainer.train(resume_from_checkpoint=checkpoint_dir)
+elif config.resume_from_checkpoint:
+    print("Warning: RESUMING FROM CHECKPOINT ON YOUR COMPUTER (not W&B)")
+    trainer.train(resume_from_checkpoint=True)
+else:
+    print("Warning: TRAINING FROM SCRATCH")
+    trainer.train()
