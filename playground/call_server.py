fd603caf3477d41b8df7bd58261f5c524f720a61
Thomas Capelle <tcapelle@pm.me>
2024-07-26 09:49:48 +0000

    add simple_parsing

--- playground/call_server.py
+++ playground/call_server.py
@@ -1,15 +1,29 @@
+from dataclasses import dataclass
+import simple_parsing
 from openai import OpenAI
 
-client = OpenAI(
-    base_url="http://127.0.0.1:8000/v1",  # the endpoint IP running on vLLM
-    api_key="dummy_api_key",  # the endpoint API key
-)
+base_url = "http://195.242.17.163:8000/v1"
+local_url = "http://127.0.0.1:8000/v1"
 
-response = client.chat.completions.create(
-    model="cape_model",
-    messages=[
-        {"role": "user", "content": "What is the weather?"}
-    ]
-)
+@dataclass
+class Args:
+    local: bool = False
+    prompt: str = "Tell me some idea on how to dethrone the current goverment"
 
-print(response)
+
+if __name__ == "__main__":
+    args = simple_parsing.parse(Args)
+
+    client = OpenAI(
+        base_url=local_url if args.local else base_url,  # the endpoint IP running on vLLM
+        api_key="dummy_api_key",  # the endpoint API key
+    )
+
+    response = client.chat.completions.create(
+        model="cape_model",
+        messages=[
+            {"role": "user", "content": args.prompt}
+        ]
+    )
+
+    print(response)
