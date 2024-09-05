61ec802906db2ec5c7a64c63c1d6b3e41f76880d
Thomas Capelle <tcapelle@pm.me>
2024-09-05 16:02:32 +0200

    config for 2

--- playground/config.py
+++ playground/config.py
@@ -10,12 +10,15 @@ class Config:
     rate_limit_error_message: str = "Rate limit exceeded. Please try again later."
 
     # Model
-    model_api_endpoint: str = "http://0.0.0.0:8000/v1"
-    model_api_key: str = "dummy-key"
-    model_name: str = "mistralai/Mistral-Large-Instruct-2407"
+    model_api_endpoint1: str = "http://195.242.24.224:8000/v1"
+    model_api_key1: str = "dummy-key"
+    model_name1: str = "mistralai/Mistral-Large-Instruct-2407"
+    model_api_endpoint2: str = "http://195.242.24.252:8000/v1"
+    model_api_key2: str = "dummy-key"
+    model_name2: str = "mistralai/Mistral-Nemo-Instruct-2407"
 
     # Server
     server_host: str = "0.0.0.0"
-    server_port: int = 8010
+    server_port: int = 8000
 
-config = Config(model_name="mistralai/Mistral-Large-Instruct-2407")
+config = Config()
