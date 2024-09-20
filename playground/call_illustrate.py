37d7e892836716af1b3819f5fe7ab61c9fef4326
Thomas Capelle <tcapelle@pm.me>
2024-09-20 09:21:42 +0200

    simple calling script

--- playground/call_illustrate.py
+++ playground/call_illustrate.py
@@ -1,17 +1,50 @@
 import requests
 import os
+from rich.console import Console
+from dataclasses import dataclass
+from simple_parsing import ArgumentParser
 
-URL = "http://localhost:8020/illustrate/story"
-WANDB_API_KEY = "1"*40
-# WANDB_API_KEY = os.environ["WANDB_API_KEY"]
+from illustrator import IllustratePayload
 
 
-illustrator_payload = dict(
-    story="Once upon a time, in a land far, far away, there was a little girl named Alice. Alice loved to explore the world around her. One day, she decided to go on an adventure to find the most beautiful flower in the world. Alice traveled through forests, over mountains, and across rivers. She met many interesting creatures along the way, like a friendly dragon and a wise old owl. After a long journey, Alice finally found the most beautiful flower in the world. She picked it and brought it back to her home. From that day on, Alice knew that with courage and determination, she could achieve anything.",
-    story_title="Alice's Flower Adventure",
-    story_author="A.A. Milne",
-    story_setting="A magical land filled with talking animals and enchanted creatures",
-)
+console = Console()
+# defaults
+LOCAL_URL = "http://localhost:8020/illustrate/story"
+REMOTE_URL = "http://195.242.25.198:8020/illustrate/story"
+WANDB_API_KEY = os.environ["WANDB_API_KEY"]
 
-response = requests.post(URL, headers={"wandb-api-key": WANDB_API_KEY}, json=illustrator_payload)
-print(response.json())
+def call_illustrate(payload: dict, url: str):
+    response = requests.post(
+        url, 
+        headers={"wandb-api-key": WANDB_API_KEY}, json=payload)
+
+    return response.json()
+
+@dataclass
+class Payload:
+    story: str = "Once upon a time, in a land far, far away, there was a little girl named Alice. Alice loved to explore the world around her. One day, she decided to go on an adventure to find the most beautiful flower in the world. Alice traveled through forests, over mountains, and across rivers. She met many interesting creatures along the way, like a friendly dragon and a wise old owl. After a long journey, Alice finally found the most beautiful flower in the world. She picked it and brought it back to her home. From that day on, Alice knew that with courage and determination, she could achieve anything."
+    story_title: str = "Alice's Flower Adventure"
+    story_author: str = "A.A. Milne"
+    story_setting: str = "A magical land filled with talking animals and enchanted creatures"
+    wandb_api_key: str = WANDB_API_KEY
+    return_images: bool = False
+    dummy: bool = False
+
+    def validate(self):
+        dict_payload = vars(self)
+        dict_payload.pop("wandb_api_key")
+        return IllustratePayload.model_validate()
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--local", action="store_true", help="Call local server")
+    parser.add_arguments(Payload, dest="payload")
+    args = parser.parse_args()
+    url = LOCAL_URL if args.local else REMOTE_URL
+    console.print(f"Sending Payload to url={url} :")
+    console.print(vars(args.payload))
+    payload = IllustratePayload.model_validate(vars(args.payload))
+    console.print("[cyan]==============================================================[/cyan]")
+    response = call_illustrate(payload.model_dump(), url)
+    console.print("Response:")
+    console.print(response)
